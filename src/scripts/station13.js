436ed314a32eda81495928ca238717576a909d55
Yudai-I <yu.yu.yu.yuudai0529@icloud.com>
2023-09-19 11:05:59 +0900

    [Finish]

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,8 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+if (check.checked == true) {
+  text.style.background = "red"
+} else {
+  text.style.background = "transparent"
+}
 }
---
+++ src/scripts/station15.js
@@ -0,0 +1,4 @@
+function displayList() {
+    const text = document.getElementById("fruits");
+    text.innerHTML ="<ul><li>リンゴ</li><li>イチゴ</li><li>ブドウ</li></ul>"
+}
